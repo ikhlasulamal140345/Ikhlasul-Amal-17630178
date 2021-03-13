82bfd800326dfb60b740c73b7aa0839770ef1d6b
ikhlasulamal140345 <ikhlasulamal103@gmail.com>
2021-03-13 08:19:16 +0700

    commit pertama project praktikum Ikhlasul Amal - 17630178

---
+++ profile.php
@@ -0,0 +1,49 @@
+ <div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/ikhlas.jpg" alt="Ikhlasul Amal" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">17630178</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Ikhlasul Amal</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 14 Maret 1999</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0816 4914 2180</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Email :</strong></td>
+                        <td align="left" width="50%">Suamalkj97@gmai.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://facebook.com/ikhlazz.kj" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/036-facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="http://instagram.com/ikhlasulamal____" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/029-instagram.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="https://wa.me/6281649142180" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/005-whatsapp.svg" width="30" alt="twitter">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
